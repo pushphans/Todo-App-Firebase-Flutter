22b1726dd924804d5c5cbfed55868b6127a28930
Pushp Hans <pushp.hans1502@gmail.com>
2025-09-21 02:23:27 +0530

    new commit

--- lib/data/services/database_service.dart
+++ lib/data/services/database_service.dart
@@ -4,12 +4,12 @@ import 'package:todo_app_firebase_flutter/data/models/todos.dart';
 class DatabaseService {
   static const String _todoReferenceName = "todos";
 
-  late final _todoRef;
+  late final CollectionReference _todoRef;
 
   DatabaseService() {
     _todoRef = FirebaseFirestore.instance
         .collection(_todoReferenceName)
-        .withConverter(
+        .withConverter<Todos>(
           fromFirestore: (snapshot, options) =>
               Todos.fromJson(snapshot.data()!),
           toFirestore: (todo, options) => todo.toJson(),
@@ -21,10 +21,14 @@ class DatabaseService {
   }
 
   void addTodo(Todos todo) async {
-    _todoRef.add(todo.toJson());
+    _todoRef.add(todo);
   }
 
   void updateTodo(String id, Todos updatedTodo) async {
     _todoRef.doc(id).update(updatedTodo.toJson());
+  }
+
+  void deleteTodo(String id) async {
+    _todoRef.doc(id).delete();
   }
 }
--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_app_firebase_flutter/data/models/todos.dart';
+import 'package:todo_app_firebase_flutter/data/services/database_service.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +12,158 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final dbService = DatabaseService();
+  final titleController = TextEditingController();
+
+  void openDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(10),
+        ),
+        title: Text("Add new Todo"),
+
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: "New Todo",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancel"),
+                ),
+
+                SizedBox(width: 5),
+
+                TextButton(
+                  onPressed: () {
+                    Todos todo = Todos(
+                      title: titleController.text,
+                      isDone: false,
+                      createdOn: Timestamp.now(),
+                    );
+
+                    // print(todo.title);
+                    // print(todo.isDone);
+                    // print(todo.createdOn);
+
+                    dbService.addTodo(todo);
+                    Navigator.pop(context);
+                    titleController.clear();
+                  },
+                  child: Text("Add"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Todos",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple.shade400,
+      ),
+
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.8,
+        width: MediaQuery.of(context).size.width,
+
+        child: StreamBuilder(
+          stream: dbService.getTodos(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Center(child: Text("Error Occurred ${snapshot.error}"));
+            }
+
+            if (snapshot.hasData) {
+              List todos = snapshot.data?.docs ?? [];
+              if (todos.isEmpty) {
+                return const Center(child: Text("Add a Todo"));
+              }
+
+              return ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  Todos todo = todos[index].data();
+                  String id = todos[index].id;
+
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 10,
+                    ),
+                    child: ListTile(
+                      onLongPress: () {
+                        dbService.deleteTodo(id);
+                      },
+                      tileColor: Colors.deepPurple.shade200,
+                      title: Text(
+                        todo.title,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+
+                          decoration: todo.isDone == true
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                        ),
+                      ),
+                      subtitle: Text(
+                        DateFormat(
+                          "dd-MM-yyyy  hh:mm a",
+                        ).format(todo.createdOn.toDate()),
+                      ),
+                      trailing: Checkbox(
+                        value: todo.isDone,
+                        onChanged: (value) {
+                          Todos updatedTodo = todo.copyWith(
+                            isDone: !todo.isDone,
+                          );
+
+                          dbService.updateTodo(id, updatedTodo);
+                        },
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+
+            return Center(child: Text("Something went wrong"));
+          },
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepPurple.shade400,
+        onPressed: openDialog,
+        child: Icon(Icons.add, color: Colors.black),
+      ),
+    );
   }
 }
